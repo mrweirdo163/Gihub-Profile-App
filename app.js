82a4f3f40370ffc59cd8c40885a35b397e1ae31e
Hvdodeptrai <hvdo21@clc.fitus.edu.vn>
2022-02-25 21:18:25 +0700

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-getUser('Hvdodeptrai');
 
 async function getUser(username) {
     const resp = await fetch(APIURL + username);
